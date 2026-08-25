8042e58f1692de3fddb24437eb2a026c46916617
ritesh-18-hiresense <ritesh@hiresense.ai>
2026-08-25 18:04:21 +0530

    update the nested router logic in context.ts file

--- src/routers/trpc/context.ts
+++ src/routers/trpc/context.ts
@@ -3,3 +3,15 @@ import { initTRPC } from "@trpc/server";
 const t=initTRPC.create();
 export const appRouter=t.router
 export const appProcedure=t.procedure;
+
+
+
+/***
+ * t.router(
+ * t.router(
+ * router: {urlRouter}
+ * )
+ * )
+ * 
+ * 
+ */
